ef3374907121c0038534f2494ff4be6f19acf6ae
Neale Taylor <nealeptaylor@gmail.com>
2023-04-27 11:43:52 -0400

    Initial Commit

---
+++ f1RaceTimes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  f1RaceTimes
+//
+//  Created by Neale Taylor on 2023-04-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ f1RaceTimes/f1RaceTimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  f1RaceTimesApp.swift
+//  f1RaceTimes
+//
+//  Created by Neale Taylor on 2023-04-27.
+//
+
+import SwiftUI
+
+@main
+struct f1RaceTimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
