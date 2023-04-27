66a5a850d3593174ef9ab48ebb0803a989d03fc2
Neale Taylor <nealeptaylor@gmail.com>
2023-04-27 14:37:50 -0400

    testing new features

--- f1RaceTimes/f1RaceTimesApp.swift
+++ f1RaceTimes/f1RaceTimesApp.swift
@@ -7,13 +7,31 @@
 import SwiftUI
 
 @main
-struct f1RaceTimesApp: App {
-  var body: some Scene {
-      MenuBarExtra {
-          
-      } label: {
-          Image(systemName: "switch.2")
-      }
-
-  }
+struct swiftui_menu_barApp: App {
+    @State var currentNumber: String = "1"
+    
+    var body: some Scene {
+        // Remove this
+        // WindowGroup {
+        //     ContentView()
+        // }
+        MenuBarExtra(currentNumber, systemImage: "\(currentNumber).circle") {
+            Button("One") {
+                currentNumber = "1"
+            }
+            .keyboardShortcut("1")
+            Button("Two") {
+                currentNumber = "2"
+            }
+            .keyboardShortcut("2")
+            Button("Three") {
+                currentNumber = "3"
+            }
+            .keyboardShortcut("3")
+            Divider()
+            Button("Quit") {
+                NSApplication.shared.terminate(nil)
+            }.keyboardShortcut("q")
+        }
+    }
 }
