9e174225af99ffdf246f0e5765f694dcb2f2f8b7
Neale Taylor <nealeptaylor@gmail.com>
2023-05-03 16:12:16 -0400

    testing info changes

--- f1RaceTimes/Race.swift
+++ f1RaceTimes/Race.swift
@@ -18,23 +18,23 @@ let sampleMenuItems = [ MenuItem(name: "Bahrain", image: "bahrainFlag", subMenuI
                         MenuItem(name: "Saudi Arabia", image: "saudiFlag", subMenuItems: saudiMenuItems),
                         MenuItem(name: "Australia", image: "australiaFlag", subMenuItems: australiaMenuItems)
                     ]
-// Sub-menu items for Espressco Machines
-let bahrainMenuItems = [ MenuItem(name: "Leva", image: "leva-x", subMenuItems: [ MenuItem(name: "Leva X", image: "leva-x"), MenuItem(name: "Leva S", image: "leva-s") ]),
-                                 MenuItem(name: "Strada", image: "strada-ep", subMenuItems: [ MenuItem(name: "Strada EP", image: "strada-ep"), MenuItem(name: "Strada AV", image: "strada-av"), MenuItem(name: "Strada MP", image: "strada-mp"), MenuItem(name: "Strada EE", image: "strada-ee") ]),
-                                 MenuItem(name: "KB90", image: "kb90"),
-                                 MenuItem(name: "Linea", image: "linea-pb-x", subMenuItems: [ MenuItem(name: "Linea PB X", image: "linea-pb-x"), MenuItem(name: "Linea PB", image: "linea-pb"), MenuItem(name: "Linea Classic", image: "linea-classic") ]),
-                                 MenuItem(name: "GB5", image: "gb5"),
-                                 MenuItem(name: "Home", image: "gs3", subMenuItems: [ MenuItem(name: "GS3", image: "gs3"), MenuItem(name: "Linea Mini", image: "linea-mini") ])
-                                ]
 
-// Sub-menu items for Grinder
+// Sub-menu items for Bahrain GP
+let bahrainMenuItems = [ MenuItem(name: "FP1", image: "leva-x"),
+                         MenuItem(name: "FP2", image: "strada-ep"),
+                         MenuItem(name: "FP3", image: "kb90"),
+                         MenuItem(name: "Qualification", image: "linea-pb-x"),
+                         MenuItem(name: "Race", image: "gb5"),
+                        ]
+
+// Sub-menu items for Saudi GP
 let saudiMenuItems = [ MenuItem(name: "Swift", image: "swift"),
                          MenuItem(name: "Vulcano", image: "vulcano"),
                          MenuItem(name: "Swift Mini", image: "swift-mini"),
                          MenuItem(name: "Lux D", image: "lux-d")
                         ]
 
-// Sub-menu items for other equipment
+// Sub-menu items for Australia GP
 let australiaMenuItems = [ MenuItem(name: "Espresso AV", image: "espresso-av"),
                          MenuItem(name: "Espresso EP", image: "espresso-ep"),
                          MenuItem(name: "Pour Over", image: "pourover"),
