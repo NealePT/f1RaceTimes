910eba4521d17ea5e51c17b22477253dca17d8af
Neale Taylor <nealeptaylor@gmail.com>
2023-05-05 15:13:07 -0400

    spacing

--- f1RaceTimes/f1RaceTimesApp.swift
+++ f1RaceTimes/f1RaceTimesApp.swift
@@ -21,11 +21,13 @@ struct swiftui_menu_barApp: App {
                         .resizable()
                         .scaledToFit()
                         .frame(width: 25, height: 25)
+                        .padding(.leading, 10)
                     
                     Text(item.name)
                         .font(.system(.title3, design: .rounded))
                         .frame(maxWidth: .infinity, alignment: .leading)
                         .bold()
+                        .padding(.leading, 5)
                     
                     Text(item.time != "" ? item.time : "")
                         .font(.system(.body))
@@ -37,6 +39,7 @@ struct swiftui_menu_barApp: App {
                 }
             }.frame(minWidth: 500)
             Divider()
+            
             Button("Quit") {
                 NSApplication.shared.terminate(nil)
             }.padding()
