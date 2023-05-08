ec1b2d16c5b907a1bb48629dfda77c512d70ce9d
Neale Taylor <nealeptaylor@gmail.com>
2023-05-08 16:05:48 -0400

    add new races to list

--- f1RaceTimes/Race.swift
+++ f1RaceTimes/Race.swift
@@ -19,6 +19,12 @@ struct MenuItem: Identifiable {
 let races = [ MenuItem(name: "Bahrain", image: "bahrainFlag", date: "March 3 - 5", time: "", subMenuItems: bahrainMenuItems),
                         MenuItem(name: "Saudi Arabia", image: "saudiFlag", date: "March 17 - 19", time: "",subMenuItems: saudiMenuItems),
                         MenuItem(name: "Australia", image: "australiaFlag", date: "March 31 - April 2", time: "",subMenuItems: australiaMenuItems),
+                        MenuItem(name: "Azerbaijan", image: "azerbaijanFlag", date: "April 28 - April 30", time: "",subMenuItems: azerbaijanMenuItems),
+                        MenuItem(name: "Miami", image: "usFlag", date: "May 5 - May7", time: "",subMenuItems: miamiMenuItems),
+                        MenuItem(name: "Imola", image: "italyFlag", date: "May 19 - May 21", time: "",subMenuItems: imolaMenuItems),
+                        MenuItem(name: "Monaco", image: "monacoFlag", date: "May 26 - May 28", time: "",subMenuItems: monacoMenuItems),
+                        MenuItem(name: "Spain", image: "spainFlag", date: "June 2 - June 4", time: "",subMenuItems: spainMenuItems),
+                        MenuItem(name: "Canada", image: "canadaFlag", date: "June 16 - June 18", time: "",subMenuItems: canadaMenuItems),
                     ]
 
 // Sub-menu items for Bahrain GP
